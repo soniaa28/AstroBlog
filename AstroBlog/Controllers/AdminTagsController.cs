c2ad10f9562f16714abfa3940474f6d450e31afc
soniaa28 <sofija.demchuk@gmail.com>
2024-03-06 19:09:49 +0200

    added edit and delete button for tags

--- AstroBlog/Controllers/AdminTagsController.cs
+++ AstroBlog/Controllers/AdminTagsController.cs
@@ -41,5 +41,54 @@ namespace AstroBlog.Controllers
             var tags = astroBlogDbContext.Tags.ToList();
             return View(tags);      
         }
+
+        [HttpGet]
+        public IActionResult Edit(Guid id) 
+        {
+            var tag = astroBlogDbContext.Tags.FirstOrDefault(t => t.Id == id);
+            if (tag != null)
+            {
+                var editTagRequest = new EditTagRequest
+                {
+                    Id = tag.Id,
+                    Name = tag.Name,
+                    DisplayName = tag.DisplayName
+                };
+                return View(editTagRequest);
+            }
+
+            return View(null);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(EditTagRequest editTagrequest) 
+        {
+            var tag = new Tag {Id= editTagrequest.Id, Name = editTagrequest.Name, DisplayName = editTagrequest.DisplayName };
+            var existingTag = astroBlogDbContext.Tags.Find(tag.Id);
+            if(existingTag != null)
+            {
+                existingTag.Name = tag.Name;
+                existingTag.DisplayName = tag.DisplayName;
+                astroBlogDbContext.SaveChanges();
+                //show success 
+                return RedirectToAction("Edit", new { id = editTagrequest.Id });
+
+            }
+            //show error
+            return RedirectToAction("Edit", new {id = editTagrequest.Id });
+        }
+        [HttpPost]
+        public IActionResult Delete(EditTagRequest editTagrequest) 
+        {
+            var tag = astroBlogDbContext.Tags.Find(editTagrequest.Id);
+            if (tag != null) 
+            {
+                astroBlogDbContext.Tags.Remove(tag);
+                astroBlogDbContext.SaveChanges();
+                return RedirectToAction("List");
+
+            }
+            return RedirectToAction("Edit", new {id = editTagrequest.Id});
+        }
     }
 }
---
+++ AstroBlog/Models/ViewModel/EditTagRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace AstroBlog.Models.ViewModel
+{
+    public class EditTagRequest
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
