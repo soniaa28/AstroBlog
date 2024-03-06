2312b2cf6b4ca523cad1379b3af0a0da5cdcdf03
soniaa28 <sofija.demchuk@gmail.com>
2024-03-06 18:30:14 +0200

    Add project files.

---
+++ AstroBlog/Controllers/AdminTagsController.cs
@@ -0,0 +1,45 @@
+﻿using AstroBlog.Data;
+using AstroBlog.Models.Domain;
+using AstroBlog.Models.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AstroBlog.Controllers
+{
+    public class AdminTagsController : Controller
+    {
+        private readonly AstroBlogDbContext astroBlogDbContext;
+        public AdminTagsController(AstroBlogDbContext astroBlogDbContext) 
+        {
+            this.astroBlogDbContext = astroBlogDbContext;
+        }
+
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Add")]
+        public IActionResult Add(AddTagRequest addTagrequest)
+        {
+            //maping addrequest to tag domain model
+            var tag = new Tag
+            {
+                Name = addTagrequest.Name,
+                DisplayName = addTagrequest.DisplayName,
+            };
+            astroBlogDbContext.Tags.Add(tag);
+            astroBlogDbContext.SaveChanges();
+            return View("Add");
+        }
+
+        [HttpGet]
+        public IActionResult List() 
+        {
+            // use dbcontet to view list
+            var tags = astroBlogDbContext.Tags.ToList();
+            return View(tags);      
+        }
+    }
+}
---
+++ AstroBlog/Data/AstroBlogDbContext.cs
@@ -0,0 +1,14 @@
+﻿using AstroBlog.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace AstroBlog.Data
+{
+    public class AstroBlogDbContext : DbContext
+    {
+        public AstroBlogDbContext(DbContextOptions<AstroBlogDbContext> options) : base(options) { }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
